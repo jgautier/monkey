15ea068598260fb40cff6865b28489d795f2104f
Julian Gautier <juliangautier@juliangautier.com>
2019-07-10 21:54:07 -0700

    major reorg and parser skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+mod lexer;
+mod ast;
+use std::io;
+use std::io::Write;
+
+const PROMPT:&str = "🐒 >>> ";
+
+fn print_prompt() {
+   print!("{}", PROMPT);
+   io::stdout().flush().unwrap();
+}
+
+fn start() {
+    loop {
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(n) => {
+                let lexer = lexer::Lexer::new(&input);
+                for token in lexer {
+                    println!("{}", token);
+                }
+                print_prompt();
+            }
+            Err(error) => println!("error: {}", error),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    print_prompt();
+    start();
 }
