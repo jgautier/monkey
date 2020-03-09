748f9109e598758cf4d1d5ddcce1aaca72466bb2
Julian Gautier <juliangautier@juliangautier.com>
2020-03-08 17:38:46 -0700

    add wasm/js resources

---
+++ pkg/monkey.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} prog 
+* @returns {string} 
+*/
+export function eval_program(prog: string): string;
+
+/**
+* If `module_or_path` is {RequestInfo}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {RequestInfo | BufferSource | WebAssembly.Module} module_or_path
+*
+* @returns {Promise<any>}
+*/
+export default function init (module_or_path?: RequestInfo | BufferSource | WebAssembly.Module): Promise<any>;
+        
